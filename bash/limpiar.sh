8f5244c0ac658daca3be67ab10f43b972037f3ce
Javier Tassi <tassij@maipu.gob.ar>
2024-03-15 09:39:56 -0300

    cambios en tiempos de purga

--- bash/limpiar.sh
+++ bash/limpiar.sh
@@ -1,5 +1,8 @@
-## Borrar los clips a los 10 dias de su creacion
-find clips/ -mmin +14400 -type f -delete
+## Borrar los clips a los 30 dias de su creacion
+find clips/ -mmin +43200 -type f -delete
+
+## Borrar los clips de larga duracion a los 6 meses de su creacion
+find clips/ -mmin +259200 -type f -delete
 
 ## Borrar los videos originales a las 2 horas de su creacion
-find downloads/ -mmin +14400 -type f -delete
+find downloads/ -mmin +120 -type f -delete
