0814ce18bb0d79ae4fc332dd59404a80ecaf767f
Javier Tassi <tassij@maipu.gob.ar>
2024-03-15 14:04:43 -0300

    fixed limpiar.sh paths

--- bash/limpiar.sh
+++ bash/limpiar.sh
@@ -1,8 +1,8 @@
 ## Borrar los clips a los 30 dias de su creacion
-find clips/ -mmin +43200 -type f -delete
+find /opt/clipweb/web/clips/ -mmin +43200 -type f -delete
 
 ## Borrar los clips de larga duracion a los 6 meses de su creacion
-find clips/ -mmin +259200 -type f -delete
+find /opt/clipweb/web/clips-permanentes/ -mmin +259200 -type f -delete
 
 ## Borrar los videos originales a las 2 horas de su creacion
-find downloads/ -mmin +120 -type f -delete
+find /opt/clipweb/web/downloads/ -mmin +120 -type f -delete
