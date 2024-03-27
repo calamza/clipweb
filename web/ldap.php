5c06aa5a91d2fae9222d773e4c38f949855ee455
Javier Tassi <tassij@maipu.gob.ar>
2024-03-27 18:52:26 -0300

    Added ldap auth

--- web/ldap.php
+++ web/ldap.php
@@ -6,14 +6,14 @@ $password = $_POST['password'];
 $ldapconfig['host'] = "unomedios.com.ar";//CHANGE THIS TO THE CORRECT LDAP SERVER
 $ldapconfig['port'] = "389";
 $ldapconfig['basedn'] = "DC=unomedios,DC=com,DC=ar";//CHANGE THIS TO THE CORRECT BASE DN
-$ldapconfig['usersdn'] = "OU=Medios";//CHANGE THIS TO THE CORRECT USER OU/CN
+$ldapconfig['usersdn'] = "OU=UNO MEDIOS S.A.,OU=UnoMedios S.A.,OU=Medios";//CHANGE THIS TO THE CORRECT USER OU/CN
 $ds=ldap_connect($ldapconfig['host'], $ldapconfig['port']);
 
 ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
 ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
 ldap_set_option($ds, LDAP_OPT_NETWORK_TIMEOUT, 10);
 
-$dn="cn=".$username.",".$ldapconfig['usersdn'].",".$ldapconfig['basedn'];
+$dn="uid=".$username.",".$ldapconfig['usersdn'].",".$ldapconfig['basedn'];
 if(isset($_POST['username'])){
 if ($bind=ldap_bind($ds, $dn, $password)) {
   echo("Login correct");//REPLACE THIS WITH THE CORRECT FUNCTION LIKE A REDIRECT;
