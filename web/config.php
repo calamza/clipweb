ccae9491ac09541ff82e9434642caa564de16644
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 19:13:08 -0300

    fix insert into

--- web/config.php
+++ web/config.php
@@ -11,6 +11,10 @@
    $db_password="Lg3681Lgs";
    $db_database="ingesta_db";
    $db_link=mysqli_connect($db_server, $db_username, $db_password,$db_database);
+   if($db_link = false) {
+      die("Error: " . mysqli_error_connect());
+   }    
+
    if (!$db_link) {
       die("Connection failed: " . mysqli_connect_error());
    }
