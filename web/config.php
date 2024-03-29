4a8620c7f8d9b42b80d762ed74141b90195ce8a4
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 15:19:29 -0300

    fix volver y sign out en cortar.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,14 @@
 <?php
+   /*
    define('DB_SERVER', 'db:3306');
    define('DB_USERNAME', 'ingesta_web');
    define('DB_PASSWORD', 'Lg3681Lg');
    define('DB_DATABASE', 'ingesta_db');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   */
+   $db_server="db:3306";
+   $db_username="ingesta_web";
+   $db_password="Lg3681Lg";
+   $db_database="ingesta_db";
+   $db=mysqli_connect($db_server, $db_username, $db_password,$db_database);
 ?>
