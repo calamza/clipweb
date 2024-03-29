d9b5a0b8474e095a9a5c9979368f8ab9a8cc8cb3
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 19:44:30 -0300

    fix insert into

--- web/cortar.php
+++ web/cortar.php
@@ -58,7 +58,7 @@ if ($permanente==0) {
     //echo $descripcion;
     
     //$sql = "INSERT INTO links (id, usuario, descripcion, link) VALUES ('', '$login_session', '$descripcion','download.php?url=clips/clip-$videoid-$randomID.mp4')";
-    $sql = "INSERT INTO links (usuario, descripcion) values (javier, kalkakakaka)";
+    $sql = "INSERT INTO links (usuario, descripcion) values ('javier', 'kalkakakaka')";
     save_history($sql);
 } else {
     shell_exec("ffmpeg -i downloads/".$videoid.".mp4  -ss ".$inicio." -to ".$fin." -c:v copy -c:a copy clips-permanentes/clip-".$videoid."-".$randomID.".mp4");
