1de9312f27ef6370ca375fdca69f301df8545b95
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 11:41:43 -0300

    fixes

--- web/ldap.php
+++ web/ldap.php
@@ -67,9 +67,11 @@ if(isset($_POST['username']) && isset($_POST['password'])){
     
 
 }else{
+if ($_SESSION['login_user'] == NULL )
+{
 ?>
     <form action="#" method="POST">
         <label for="username">Username: </label><input id="username" type="text" name="username" /> 
         <label for="password">Password: </label><input id="password" type="password" name="password" />        <input type="submit" name="submit" value="Submit" />
     </form>
-<?php } ?> 
+<?php } } ?> 
