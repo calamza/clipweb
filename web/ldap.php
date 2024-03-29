0dd5fde0d4d87c75048aabd68580e683d02b9958
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 09:42:29 -0300

    fixes

--- web/ldap.php
+++ web/ldap.php
@@ -20,7 +20,7 @@ if(isset($_POST['username']) && isset($_POST['password'])){
     if ($bind) {
         $filter="(sAMAccountName=$username)";
         $result = ldap_search($ldap,"dc=unomedios,dc=com,dc=ar",$filter);
-        //ldap_sort($ldap,$result,"sn");
+        ldap_sort($ldap,$result,"sn");
         //$info = ldap_get_entries($ldap, $result);
         echo "entro!";
         /*
