e1c18e3985ebe1e491362866e277aff72e5bd9d0
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 10:18:29 -0300

    fixes

--- web/ldap.php
+++ web/ldap.php
@@ -19,10 +19,7 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 
     if ($bind) {
         $filter="(sAMAccountName=$username)";
-        $ADGroup="G_SI";
-        $filter_group="(&(memberOf=" .$ADGroup. "))";
-        $filter=$filter.$filter_group;
-        $result = ldap_search($ldap,"dc=unomedios,dc=com,dc=ar",$filter);
+        $result = ldap_search($ldap,"dc=unomedios,dc=com,dc=ar",$filter,array("memberof"));
         //ldap_sort($ldap,$result,"sn");
         $info = ldap_get_entries($ldap, $result);
         echo "entro!";
