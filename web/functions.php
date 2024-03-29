8d4c91028605c37290cc3d85648c94fa4f926fc3
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 16:17:32 -0300

    create functions

--- web/functions.php
+++ web/functions.php
@@ -1,5 +1,13 @@
 <?php
-include('config.php');
+//include('config.php');
+$db_server="db:3306";
+$db_username="ingesta_web";
+$db_password="Lg3681Lg";
+$db_database="ingesta_db";
+$db=mysqli_connect($db_server, $db_username, $db_password,$db_database);
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 function mostrarTexto($texto) {
     
     if (mysqli_query($db, $texto)) {
