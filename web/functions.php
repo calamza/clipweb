06c8f833ba0c5dce599174c984bbc3bb7014ecc5
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 18:59:18 -0300

    fix insert into

--- web/functions.php
+++ web/functions.php
@@ -3,7 +3,7 @@ include('config.php');
 function save_history($texto) {
     $texto = "INSERT INTO links ('descripcion') VALUES ('pruebaaa')";
     
-    $result = mysqli_query($db, "INSERT INTO links ('descripcion') VALUES ('pruebaaa')");
+    $result = mysqli_query($db, "SELECT * FROM links");
     print_r($result);
     die("waiting...");
     if ($result === false) {
