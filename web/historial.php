a7e6c5807495eaac6c9abf1f6df9b75aac1122cd
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 20:03:26 -0300

    added history

--- web/historial.php
+++ web/historial.php
@@ -21,7 +21,7 @@ include('config.php');
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
-          echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+          echo "usuario: " . $row["usuario"]. " - descripcion: " . $row["link"]. "<br>";
         }
       } else {
         echo "0 results";
