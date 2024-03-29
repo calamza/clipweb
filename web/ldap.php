d82a7c6ded4f8c64d1414a1eac08a9d1b8654b1f
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 09:33:30 -0300

    change ldap.php

--- web/ldap.php
+++ web/ldap.php
@@ -1,20 +1,47 @@
 <?php
+/**
+ * Created by Joe of ExchangeCore.com
+ */
+if(isset($_POST['username']) && isset($_POST['password'])){
 
-$server = "unomedios.com.ar";  //this is the LDAP server you're connecting with
-$ds = ldap_connect("ldap://$server", 389); //always connect securely via LDAPS when possible
+    $adServer = "ldap://unomedios.com.ar";
 
-ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
-ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
+    $ldap = ldap_connect($adServer);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$dn = "CN=Javier Alberto Tassi,OU=UNO MEDIOS S.A.,OU=UnoMedios S.A.,OU=Medios,DC=unomedios,DC=com,DC=ar";
-$pass = 'Aa12162389"';
-$ldapbind = ldap_bind($ds, $dn, $pass); //this is the point we are authenticating
+    $ldaprdn = 'mydomain' . "\\" . $username;
 
-$dn = "CN=G_Administracion_OU,OU=Grupos_Medios,OU=Medios,DC=unomedios,DC=com,DC=ar"; //very important: in which part of your database are you looking
-$filter = "cn=*"; //don't filter anyone out (every user has a uid)
-$sr = ldap_search($ds, $dn, $filter) or die ("bummer"); //define your search scope
+    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
 
-$results = ldap_get_entries($ds, $sr); //here we are pulling the actual entries from the search we just defined
-var_dump($results); //will give you all results is array form. 
+    $bind = @ldap_bind($ldap, $ldaprdn, $password);
 
+    if ($bind) {
+        $filter="(sAMAccountName=$username)";
+        $result = ldap_search($ldap,"dc=unomedios,dc=com,dc=ar",$filter);
+        ldap_sort($ldap,$result,"sn");
+        $info = ldap_get_entries($ldap, $result);
+        for ($i=0; $i<$info["count"]; $i++)
+        {
+            if($info['count'] > 1)
+                break;
+            echo "<p>You are accessing <strong> ". $info[$i]["sn"][0] .", " . $info[$i]["givenname"][0] ."</strong><br /> (" . $info[$i]["samaccountname"][0] .")</p>\n";
+            echo '<pre>';
+            var_dump($info);
+            echo '</pre>';
+            $userDn = $info[$i]["distinguishedname"][0]; 
+        }
+        @ldap_close($ldap);
+    } else {
+        $msg = "Invalid email address / password";
+        echo $msg;
+    }
+
+}else{
 ?>
+    <form action="#" method="POST">
+        <label for="username">Username: </label><input id="username" type="text" name="username" /> 
+        <label for="password">Password: </label><input id="password" type="password" name="password" />        <input type="submit" name="submit" value="Submit" />
+    </form>
+<?php } ?> 
