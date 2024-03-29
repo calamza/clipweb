55671629a09fed2c729c4bdb94f48577934f0edd
Javier Tassi <tassij@maipu.gob.ar>
2024-03-29 20:09:56 -0300

    added history

--- web/functions.php
+++ web/functions.php
@@ -7,7 +7,7 @@ function save_history($texto) {
     //$result = mysqli_query($db_link, "SELECT * FROM links");
     
     if (mysqli_query($db_link, $texto)) {
-        echo "<strong>Link guardado en mi historial</strong>\n\n\n";  
+        echo "<h3>Link guardado en mi historial</h3>\n\n\n";  
     }else{  
         echo "Could not insert record: ". mysqli_error($db_link);  
     }
