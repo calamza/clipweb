19a49d791848075f38707f192ee2fcd88b87724a
Javier Tassi <tassij@maipu.gob.ar>
2024-04-02 14:52:00 -0300

    added links to history of all users

--- web/historial.php
+++ web/historial.php
@@ -25,7 +25,7 @@ if (isset($users)) {
 <h2><a href="index.php">Ir al home</a></h2>
 <?php
   if ($user = "todos") {
-    echo "<h2>Historico de links creados por ".$login_session ."</h2>";
+    echo "<h2>Historico de links creados por todos</h2>";
   }
   else {
     echo "<h2>Historico de links creados por ".$login_session ."</h2>";
