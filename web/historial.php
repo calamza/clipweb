2d22ac005a004d7fa844a83d6deee39f4d1fea74
Javier Tassi <tassij@maipu.gob.ar>
2024-04-02 11:45:28 -0300

    added function check clip alive

--- web/historial.php
+++ web/historial.php
@@ -3,7 +3,7 @@
 include('ldap.php');
 include('session.php');
 include('config.php');
-include('function.php');
+include('functions.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,14 +25,14 @@ include('function.php');
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
-          check_clip_existance($row["link"]);
-          /*
+          //check_clip_existance($row["link"]);
+          
           if (check_clip_existance($row["link"]) == 1) {
             echo "existe todavia";
           } else {
             echo "link vencido";
           }
-          */
+          
           $link_download="https://clipcms.unomedios.com.ar/".$row["link"];
           echo "<tr><td>".$row["descripcion"]."</td><td><a href='".$link_download. "'>Descargar clip</a></td><td><a href='https://mediacms.unomedios.com.ar/view?m=".$row["videoid"]."'>Ir al original</a></td><td>".$row["inicio"]. "</td><td>".$row["fin"]. "</td></tr>";
         }
