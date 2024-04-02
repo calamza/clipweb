0939d29c30094c3c132d0a995cf03e11e632d5ee
Javier Tassi <tassij@maipu.gob.ar>
2024-04-02 11:41:25 -0300

    added function check clip alive

--- web/functions.php
+++ web/functions.php
@@ -44,7 +44,7 @@ function check_clip_existance($url){
     // Creating a variable with an URL 
     // to be checked
 
-    $url = "https://clipcms.unomedios.com.ar/".substr($url,17);; 
+    $url = "https://clipcms.unomedios.com.ar/".substr($url,17);
     echo $url;
     // Getting page header data 
     $array = @get_headers($url); 
