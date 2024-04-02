2c92e717e62325ce1eea0658f4264cd173a241a7
Javier Tassi <tassij@maipu.gob.ar>
2024-04-02 20:06:57 -0300

    added footer

--- web/footer.php
+++ web/footer.php
@@ -5,7 +5,7 @@
 <p>
 version=1.0.0</br>
 Para uso de Grupo America</br>
-Contacto: <a href="mailto:soporte@grupoamerica.com.ar"></br>
+Contacto: <a href="mailto:soporte@grupoamerica.com.ar">soporte@grupoamerica.com.ar</a></br>
 <p>
 </footer>
 </body>
