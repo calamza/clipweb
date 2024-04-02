c2139d539898531fe2cdce92b629c4948efc027e
Javier Tassi <tassij@maipu.gob.ar>
2024-04-02 12:13:08 -0300

    fix listing historial

--- web/historial.php
+++ web/historial.php
@@ -32,11 +32,15 @@ $filtro = $_GET['filtro'];
           //check_clip_existance($row["link"]);
           
           if (check_clip_existance($row["link"]) == 1) {
-            $link_download="https://clipcms.unomedios.com.ar/".$row["link"];
-            echo "<tr><td>".$row["descripcion"]."</td><td><a href='".$link_download. "'>Descargar clip</a></td><td><a href='https://mediacms.unomedios.com.ar/view?m=".$row["videoid"]."'>Ir al original</a></td><td>".$row["inicio"]. "</td><td>".$row["fin"]. "</td></tr>";
+            if ($filtro == "activo" or $filtro == NULL) {
+              $link_download="https://clipcms.unomedios.com.ar/".$row["link"];
+              echo "<tr><td>".$row["descripcion"]."</td><td><a href='".$link_download. "'>Descargar clip</a></td><td><a href='https://mediacms.unomedios.com.ar/view?m=".$row["videoid"]."'>Ir al original</a></td><td>".$row["inicio"]. "</td><td>".$row["fin"]. "</td></tr>";
+            }
           } else {
-            $link_download="https://clipcms.unomedios.com.ar/".$row["link"];
-            echo "<tr><td>".$row["descripcion"]."</td><td><p>Link vencido!!</p></td><td><a href='https://mediacms.unomedios.com.ar/view?m=".$row["videoid"]."'>Ir al original</a></td><td>".$row["inicio"]. "</td><td>".$row["fin"]. "</td></tr>";
+            if ($filtro == "vencido" or $filtro == NULL) {
+              $link_download="https://clipcms.unomedios.com.ar/".$row["link"];
+              echo "<tr><td>".$row["descripcion"]."</td><td><p>Link vencido!!</p></td><td><a href='https://mediacms.unomedios.com.ar/view?m=".$row["videoid"]."'>Ir al original</a></td><td>".$row["inicio"]. "</td><td>".$row["fin"]. "</td></tr>";
+            }
           }
           
           
