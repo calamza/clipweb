ffb07d6c131306c7eba797542715dd671bfcfbdd
Javier Tassi <tassij@maipu.gob.ar>
2024-04-02 14:09:37 -0300

    added links to history of all users

--- web/historial.php
+++ web/historial.php
@@ -34,7 +34,7 @@ if (isset($users)) {
     <?php
     
     if ($users == NULL) {
-      $select = "SELECT usuario,descripcion,link,videoid,inicio,fin FROM links WHERE usuario=$_SESSION['login_user']";
+      //$select = "SELECT usuario,descripcion,link,videoid,inicio,fin FROM links WHERE usuario=$_SESSION['login_user']";
       //$result = mysqli_query($db_link, "SELECT usuario,descripcion,link,videoid,inicio,fin FROM links WHERE usuario=$_SESSION['login_user']");
     } else {
       $select = "SELECT usuario,descripcion,link,videoid,inicio,fin FROM links";
